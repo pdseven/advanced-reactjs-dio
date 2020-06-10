7c40e2f30ce72c6b083c64294c28fa8ab2f561bc
Diego Neves <pdcassiano@gmail.com>
2020-06-10 17:50:37 -0300

    primeira aula de react avançado

---
+++ src-contextapi-1/Form.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Form() {
+
+  return (
+    <form>
+      <label>Nome:</label>
+      <input />
+      <label>E-mail:</label>
+      <input />
+      <label>Idade:</label>
+      <input />
+      <label>Telefone:</label>
+      <input />
+    </form>
+  )
+}
+
+export default Form
---
+++ src-hooks/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import Twitter from './Twitter'
+
+class App extends Component {
+
+  state = {
+    loading: false,
+    actived: true
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        loading: true
+      })
+    }, 3000)
+  }
+
+  onRemove = () => {
+    this.setState({
+      actived: false
+    })
+  }
+
+  render() {
+
+    const posts = [{
+      title: 'xpto',
+      descriptio: 'foo'
+    }, {
+      title: 'xpto2',
+      descriptio: 'foo2'
+    }]
+
+    return (
+      <div>
+        <button onClick={this.onRemove}>Remover Component</button>
+        {this.state.actived && <Twitter posts={posts} loading={this.state.loading} />}
+      </div>
+    )
+  }
+}
+
+export default App
